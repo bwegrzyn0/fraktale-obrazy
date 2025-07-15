d75b5dd3dd88fe2f280c9800ce2b6f2e1faeb6f8
bwegrzyn0 <bwegrzyn@duck.com>
2025-07-15 16:04:16 +0200

    naprawione i dodane dodawanie punktów do zbioru

--- src/main.cpp
+++ src/main.cpp
@@ -53,7 +53,7 @@ void handleEvents() {
 
 // x'=ax*x+bx*y+cx
 // y'=ay*x+by*y+cy
-float[] affineTransform(float ax, float bx, float ay, float by, float cx, float cy, float x, float y) {
+float* affineTransform(float ax, float bx, float ay, float by, float cx, float cy, float x, float y) {
 	float new_x = ax * x + bx * y + cx;
 	float new_y = ay * x + by * y + cy;
 	float result[] = {new_x, new_y};
@@ -64,20 +64,30 @@ float[] affineTransform(float ax, float bx, float ay, float by, float cx, float 
 int n = 4; // liczba transformacji
 // wszystkie parametry transformacji
 // dla paproci Barnsleya
-float ax[n] = {0.24, 0.22, 0.14, 0.8};
-float bx[n] = {-0.007, -0.33, -0.36, 0.1};
-float ay[n] = {0.007, 0.36, -0.38, -0.1};
-float by[n] = {0.015, 0.1, -0.1, 0.8};
-float cx[n] = {0, 0.54, 1.4, 1.6};
-float cy[n] = {0, 0, 0, 0};
+float ax[4] = {0.24, 0.22, 0.14, 0.8};
+float bx[4] = {-0.007, -0.33, -0.36, 0.1};
+float ay[4] = {0.007, 0.36, -0.38, -0.1};
+float by[4] = {0.015, 0.1, -0.1, 0.8};
+float cx[4] = {0, 0.54, 1.4, 1.6};
+float cy[4] = {0, 0, 0, 0};
 
 int N = 10; // liczba iteracji
 	   
 // zbiór punktów
 std::vector<float> x, y;
+int N_points_sqrt = 100; // pierwiastek kwadratowy liczby punktów 
 
+// dodajemy punkty do zbioru
 void setupSet() {
-
+	// równomiernie rozłożone w kwadracie o boku 1	
+	float sideLength = 1.0f;
+	float spacing = sideLength / (float) N_points_sqrt;
+	for (int i = 0; i < N_points_sqrt; i++) {
+		for (int j = 0; j < N_points_sqrt; j++) {
+			x.push_back(sideLength * (float) i);
+			y.push_back(sideLength * (float) j);
+		}
+	}
 }
 
 void generateFractal() {
@@ -95,6 +105,7 @@ void draw() {
 void loop() {
 	while (running) {
 		handleEvents();
+		draw();
 	}
 }
 
@@ -102,6 +113,7 @@ int main(int argc, char* argv[]) {
 	if (!init()) {
 		return 1;
 	}
+	setupSet();
 	generateFractal();
 	loop();
 	close();
