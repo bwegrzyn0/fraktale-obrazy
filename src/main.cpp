e78001f9813c95f91ebae07030f66e077e9c1052
bwegrzyn0 <bwegrzyn@proton.me>
2025-07-15 15:54:06 +0200

    parametry przekształcenia, przekształcenie afiniczne

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <SDL2/SDL.h>
+#include <vector>
 
 const int WIDTH=1280;
 const int HEIGHT=720;
@@ -50,18 +51,43 @@ void handleEvents() {
     }
 }
 
+// x'=ax*x+bx*y+cx
+// y'=ay*x+by*y+cy
+float[] affineTransform(float ax, float bx, float ay, float by, float cx, float cy, float x, float y) {
+	float new_x = ax * x + bx * y + cx;
+	float new_y = ay * x + by * y + cy;
+	float result[] = {new_x, new_y};
+	return result;
+}
+
+// PARAMTERY DO GENERACJI FRAKTALA
+int n = 4; // liczba transformacji
+// wszystkie parametry transformacji
+// dla paproci Barnsleya
+float ax[n] = {0.24, 0.22, 0.14, 0.8};
+float bx[n] = {-0.007, -0.33, -0.36, 0.1};
+float ay[n] = {0.007, 0.36, -0.38, -0.1};
+float by[n] = {0.015, 0.1, -0.1, 0.8};
+float cx[n] = {0, 0.54, 1.4, 1.6};
+float cy[n] = {0, 0, 0, 0};
+
+int N = 10; // liczba iteracji
+	   
+// zbiór punktów
+std::vector<float> x, y;
+
+void setupSet() {
+
+}
+
+void generateFractal() {
+}
+
 void draw() {
-	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0xFF);
 	SDL_RenderClear(renderer); 
 
-	SDL_Rect rect;
-	rect.x = 100;
-	rect.y = 100;
-	rect.w = 100;
-	rect.h = 100;
-
 	SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF); 
-	SDL_RenderFillRect(renderer, &rect); 
 
 	SDL_RenderPresent(renderer); 
 }
@@ -69,7 +95,6 @@ void draw() {
 void loop() {
 	while (running) {
 		handleEvents();
-		draw();
 	}
 }
 
@@ -77,6 +102,7 @@ int main(int argc, char* argv[]) {
 	if (!init()) {
 		return 1;
 	}
+	generateFractal();
 	loop();
 	close();
 	return 0;
